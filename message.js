e9db1ab5e011a9d94ae8bd6fd1570986a58aeef9
Pratham172 <prathamkulal19@gmail.com>
2023-10-06 19:38:50 +0530

    first commit

---
+++ message.js
@@ -0,0 +1,6 @@
+const message = () => {
+    const name = "pratham";
+    const age = 21;
+    return name + " is " + age + "years old.";
+};
+export default message;
---
+++ script.js
@@ -0,0 +1,4 @@
+const name = "pratham";
+const age = 21;
+export { name, age};
+
